4d86da480cf8f7ac869c3a24edcbe6f164353773
Tyler Whitehouse <tywhitehouse10@hotmail.com>
2015-03-24 19:20:29 -0400

    testThree added, in prime numbers

---
+++ PrimeFactors/src/primeFactors/PrimeNumbers.java
@@ -0,0 +1,12 @@
+package primeFactors;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeNumbers {
+
+	public static List<Integer> generate(Integer input) {
+		return null;
+	}
+
+}
